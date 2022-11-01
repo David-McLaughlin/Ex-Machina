c77305492cc810836bb0cf79574d73f0695ac018
David-McLaughlin <108822713+David-McLaughlin@users.noreply.github.com>
2022-11-01 09:58:31 -0400

    Update ocr.py

--- AI/ocr.py
+++ AI/ocr.py
@@ -1,1 +1,13 @@
+#####################################################
+# Simple little program to extract text from images 
+# Review README for set up
+# Executed on WSL ( Ubuntu 20.04 ) 
+#####################################################
+from PIL import Image
+import pytesseract
+import numpy as np
 
+filename = '/filepath/poem.png'
+img1 = np.array(Image.open(filename))
+text = pytesseract.image_to_string(img1)
+print(text)
